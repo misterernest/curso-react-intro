bd801cf1454223dd5f317761fc8dd3c58d11f1b3
Ernesto Campos V <misterernest@hotmail.com>
2023-11-03 15:52:48 +1100

    fix identation in src/TodoCounter.js file

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,11 +1,11 @@
 import './style/TodoCounter.css';
 
 function TodoCounter({ total, completed }) { 
-    return (
-        <h1 className='TodoCounter'>
-            Haz completado <span>{completed}</span> de <span>{total}</span> TODOs
-        </h1>
-    )
-  }
+	return (
+		<h1 className='TodoCounter'>
+			Haz completado <span>{completed}</span> de <span>{total}</span> TODOs
+		</h1>
+	)
+}
 
-  export { TodoCounter }
+export { TodoCounter }
