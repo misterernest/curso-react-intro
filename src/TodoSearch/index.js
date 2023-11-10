e89933a3777eba5b05c7a0a1b0a84b0ce7a7bec4
Ernesto Campos V <misterernest@hotmail.com>
2023-11-10 19:24:20 +1100

    refactor TodoSearch code with useContext to work without props

--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './style/TodoSearch.css';
+import { TodoContext } from '../TodoContext';
 
-function TodoSearch({
-  searchValue,
-  setSearchValue,
-}) {
+function TodoSearch() {
+  const { searchValue, setSearchValue } = React.useContext(TodoContext);
   return (
     <input
       id='todoSearch'
